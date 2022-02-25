a26aa0cd6a0d54176f0a183e4b562e8b03febf61
Victor <1209494+demosjarco@users.noreply.github.com>
2022-02-25 12:57:18 -0700

    Use this to edit later

--- classes/MainPopup.js
+++ classes/MainPopup.js
@@ -3,13 +3,13 @@ const { Menu, Tray } = require('electron');
 module.exports.MainPopup = class {
 	constructor() {
 		this.tray = new Tray('./images/logo/icon.png');
-		const contextMenu = Menu.buildFromTemplate([
+		this.contextMenu = Menu.buildFromTemplate([
 			{ label: 'Item1', type: 'radio' },
 			{ label: 'Item2', type: 'radio' },
 			{ label: 'Item3', type: 'radio', checked: true },
 			{ label: 'Item4', type: 'radio' }
 		]);
 		this.tray.setToolTip('This is my application.');
-		this.tray.setContextMenu(contextMenu);
+		this.tray.setContextMenu(this.contextMenu);
 	}
 }
