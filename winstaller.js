a5b1b1c86a8a7d84e2c1b5206b4b4ba146b6247b
Victor <1209494+demosjarco@users.noreply.github.com>
2022-04-21 13:14:05 -0700

    Generate 32bit then 64bit

--- winstaller.js
+++ winstaller.js
@@ -1,15 +1,28 @@
 'use strict';
 
 require('electron-winstaller').createWindowsInstaller({
-	appDirectory: 'builds/packaged/windows-auto-theme-win32-x64',
+	appDirectory: 'builds/packaged/windows-auto-theme-win32-ia32',
 	outputDirectory: 'builds/installers',
 	exe: 'windows-auto-theme.exe',
 	// iconUrl: 'images/logo/icon.ico',
-	setupExe: 'windows-auto-theme-installer-x64.exe',
-	// setupMsi: 'windows-auto-theme-installer-x64.msi',
+	setupExe: 'windows-auto-theme-installer-x32.exe',
+	// setupMsi: 'windows-auto-theme-installer-x32.msi',
 	noMsi: true
 }).then(() => {
-	console.log('Installer created');
+	console.log('x32 Installer created');
+	require('electron-winstaller').createWindowsInstaller({
+		appDirectory: 'builds/packaged/windows-auto-theme-win32-x64',
+		outputDirectory: 'builds/installers',
+		exe: 'windows-auto-theme.exe',
+		// iconUrl: 'images/logo/icon.ico',
+		setupExe: 'windows-auto-theme-installer-x64.exe',
+		// setupMsi: 'windows-auto-theme-installer-x64.msi',
+		noMsi: true
+	}).then(() => {
+		console.log('x64 Installer created');
+	}, (error) => {
+		throw error;
+	});
 }, (error) => {
 	throw error;
 });
