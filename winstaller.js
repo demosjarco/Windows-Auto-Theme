3329346a3ce114d632c7294def330370ef142ab5
Victor <1209494+demosjarco@users.noreply.github.com>
2022-04-21 12:47:31 -0700

    Add icon for installer

--- winstaller.js
+++ winstaller.js
@@ -4,6 +4,7 @@ require('electron-winstaller').createWindowsInstaller({
 	appDirectory: 'builds/packaged/Windows Auto Theme-win32-x64',
 	outputDirectory: 'builds/installers',
 	exe: 'Windows Auto Theme.exe',
+	// iconUrl: 'images/logo/icon.ico',
 	setupExe: 'windows-auto-theme-installer.exe',
 	setupMsi: 'windows-auto-theme-installer.msi',
 	noMsi: false
