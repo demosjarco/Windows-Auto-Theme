df87b0c4cb5db2db3e97c3fe1fb30b1d32429437
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-10-16 16:29:26 -0700

    Better error logging

--- classes/IrlTimes.js
+++ classes/IrlTimes.js
@@ -17,7 +17,22 @@ module.exports.Irl = class {
 		const axios = require('axios');
 		axios(this.#config)
 			.catch((error) => {
-				throw error;
+				if (error.response) {
+					// The request was made and the server responded with a status code
+					// that falls out of the range of 2xx
+					console.error(error.response.data);
+					console.error(error.response.status);
+					console.error(error.response.headers);
+				} else if (error.request) {
+					// The request was made but no response was received
+					// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+					// http.ClientRequest in node.js
+					console.error(error.request);
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					console.error('Error', error.message);
+				}
+				console.error(error.config);
 			})
 			.then((response) => {
 				if (response.status == 200) {
