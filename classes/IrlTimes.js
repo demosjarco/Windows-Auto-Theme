aa21e8817b06754d818a90239075e0ee13f455d9
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2022-10-16 15:46:23 -0700

    Get sr and ss from online api

---
+++ classes/IrlTimes.js
@@ -0,0 +1,30 @@
+module.exports.Irl = class {
+	#config;
+
+	constructor(locJson) {
+		this.#config = {
+			url: '/json',
+			method: 'get',
+			baseURL: 'https://api.sunrisesunset.io',
+			params: {
+				lat: locJson.lat,
+				lng: locJson.long,
+			},
+		};
+	}
+
+	getTimes(callback) {
+		const axios = require('axios');
+		axios(this.#config).catch((error) => {
+			throw error;
+		}).then((response) => {
+			if (response.status == 200) {
+				const currentDay = new Date();
+				callback({
+					sr: new Date(`${currentDay.toLocaleDateString()} ${response.data.results.sunrise}`),
+					ss: new Date(`${currentDay.toLocaleDateString()} ${response.data.results.sunset}`),
+				});
+			}
+		});
+	}
+};
