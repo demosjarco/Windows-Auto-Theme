3401296027f2483058b9aaf17d5c7deb036503b1
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-02-17 19:47:06 -0700

    left over from old code

--- src/IrlTimes.js
+++ src/IrlTimes.js
@@ -1,18 +1,8 @@
 /* eslint-disable accessor-pairs */
 module.exports.Irl = class {
-	#config;
 	#url = new URL('https://api.sunrisesunset.io');
 
 	constructor(locJson) {
-		this.#config = {
-			url: '/json',
-			method: 'get',
-			baseURL: 'https://api.sunrisesunset.io',
-			params: {
-				lat: locJson.lat,
-				lng: locJson.long,
-			},
-		};
 		this.#url.pathname = '/json';
 		this.#url.searchParams.set('lat', locJson.lat);
 		this.#url.searchParams.set('lng', locJson.long);
